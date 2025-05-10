fdf37fc57951865948d0c1533118413b5f642555
Bisan <bisan.elghoul@gmail.com>
2025-05-10 11:05:17 +0300

    update handler

--- src/components/SignUp/handlers.ts
+++ src/components/SignUp/handlers.ts
@@ -3,7 +3,6 @@ import { rest } from 'msw';
 export const handlers = [
   // Define a POST request handler
   rest.post('/your-url', (req, res, ctx) => {
-    // You can also check request body, headers, etc. here
     return res(
       ctx.status(200),
       ctx.json({ message: 'Mock response' })
